00adbd9548de89dfdedfa08fcc0f78248f29a214
saisrinivasl <38246768+saisrinivaslakkakula@users.noreply.github.com>
2022-01-05 00:36:45 +0530

    Time: 56 ms (28.46%), Space: 14.2 MB (70.75%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -1,8 +1,10 @@
 class Solution:
-    def helper(self,n)->int:
-        if n==0: return 0
-        if n==1: return 1
-        return self.helper(n-1)+self.helper(n-2)
+    def helper(self,n,memo)->int:
+        if n in memo: return memo[n]
+        ans = self.helper(n-1,memo)+self.helper(n-2,memo)
+        memo[n] = ans
+        return ans
     def fib(self, n: int) -> int:
-        return(self.helper(n))
+        memo = {0:0,1:1}
+        return(self.helper(n,memo))
         
