fa12389f7bfe8310254043c853979042960528ca
saisrinivasl <38246768+saisrinivaslakkakula@users.noreply.github.com>
2022-01-08 19:10:49 +0530

    Time: 43 ms (24.19%), Space: 14.4 MB (38.17%) - LeetHub

--- 314-binary-tree-vertical-order-traversal/314-binary-tree-vertical-order-traversal.py
+++ 314-binary-tree-vertical-order-traversal/314-binary-tree-vertical-order-traversal.py
@@ -4,33 +4,24 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-'''
-'''
 class Solution:
     def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        if not root: return[]
-        queue = deque() #S;O(n)
-        queue.append((root,0))
-        cols = {} #s;0(n)
-        max_key = min_key = 0
-        while queue: #O(n)
-            curr = queue.popleft() #O(1)
-            curr_node = curr[0]
-            curr_col = curr[1]
-            if curr_node.left: queue.append((curr_node.left,curr_col-1)) #O(1)
-            if curr_node.right: queue.append((curr_node.right,curr_col+1))
-            if not curr_col in cols: cols[curr_col] = [curr_node.val] #O(1)
-            else:cols[curr_col].append(curr_node.val) #O(1)
-            if curr_col < min_key : min_key = curr_col
-            elif curr_col > max_key: max_key = curr_col
-        res = [] #S:O(n)
-        #sorted_keys = sorted(cols.keys()) #O(n log n)
-        i = min_key
-        while i<=max_key: #O(n)
-            res.append(cols[i]) #O(n)
+        if not root: return []
+        queue = deque()
+        levels = defaultdict(list)
+        queue.append((0,root))
+        while queue:
+            curr_level,curr_node = queue.popleft()
+            levels[curr_level].append(curr_node.val)
+            if curr_node.left:
+                queue.append((curr_level-1,curr_node.left))
+            if curr_node.right:
+                queue.append((curr_level+1,curr_node.right))
+        min_idx = min(levels.keys())
+        max_idx = max(levels.keys())
+        i = min_idx
+        res = []
+        while i<= max_idx:
+            res.append(levels[i])
             i+=1
         return(res)
-        
-            
-            
-        
