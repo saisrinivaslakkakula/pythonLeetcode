22359529e05d67dca1e34b70f274606efad2aa6b
saisrinivasl <38246768+saisrinivaslakkakula@users.noreply.github.com>
2022-01-08 19:22:05 +0530

    Time: 59 ms (97.58%), Space: 18.5 MB (34.82%) - LeetHub

--- 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.py
+++ 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.py
@@ -6,13 +6,6 @@ class Node:
         self.left = None
         self.right = None
         self.parent = None
-        
-        push p,q initially to set
-        P keeps on adding it's parent to set and traverse up
-        Q also keeps on adding it's paretn and traverse up
-        at any point either p finds it's parent in set or q finds it's parent in the set, return the parent
-        in the end if none such condition is met, return none.
-        
 """
 
 class Solution:
@@ -21,12 +14,14 @@ class Solution:
         while p or q:
             if p and p.parent:
                 if p.parent in parents: return p.parent
-                else:parents.add(p.parent)
+                else: parents.add(p.parent)
                 p = p.parent
             if q and q.parent:
                 if q.parent in parents: return q.parent
                 else: parents.add(q.parent)
                 q = q.parent
-        return None
+        
+            
+        
             
         
