8cc20e3417b69b63ea0c3d138be566ac66d436f9
saisrinivasl <38246768+saisrinivaslakkakula@users.noreply.github.com>
2022-01-08 05:54:41 +0530

    Time: 55 ms (14.62%), Space: 15.1 MB (83.11%) - LeetHub

---
+++ 426-convert-binary-search-tree-to-sorted-doubly-linked-list/426-convert-binary-search-tree-to-sorted-doubly-linked-list.py
@@ -0,0 +1,34 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+"""
+
+class Solution:
+    def helper(self,node,queue):
+        if not node: return
+        self.helper(node.left,queue)
+        queue.append(node)
+        self.helper(node.right,queue)
+    def treeToDoublyList(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        if not root: return None
+        if not root.left and not root.right: 
+            root.left = root.right = root 
+            return root
+        queue = deque()
+        self.helper(root,queue)
+        head = tail = queue.popleft()
+        while queue:
+            curr = queue.popleft()
+            tail.right = curr
+            curr.right = head
+            curr.left = tail
+            tail = curr
+            head.left = tail
+        return(head)
+        
+        
+        
