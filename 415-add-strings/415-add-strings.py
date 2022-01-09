36728815e5ea24b227e5984a7beaed9f0fc3ac71
saisrinivasl <38246768+saisrinivaslakkakula@users.noreply.github.com>
2022-01-10 02:44:14 +0530

    Time: 93 ms (8.89%), Space: 14.3 MB (85.29%) - LeetHub

--- 415-add-strings/415-add-strings.py
+++ 415-add-strings/415-add-strings.py
@@ -1,39 +1,38 @@
 class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
-        i= len(num1)-1
-        j = len(num2)-1
-        cur_sum = 0
+        if len(num1)>len(num2):
+            small = num2[::-1]
+            big = num1[::-1]
+        else:
+            big= num2[::-1]
+            small = num1[::-1]
         carry = 0
-        sum_s =""
-        while i>=0 and j >=0:
-            #print(i,j)
-            cur_sum = int(num1[i])+int(num2[j])+carry
+        cur_sum = 0
+        res = ""
+        for i,d in enumerate(small):
+            cur_sum=carry+int(d)+int(big[i])
             if cur_sum >9:
-                sum_s += str(cur_sum%10)
                 carry = 1
+                res+=str(cur_sum%10)
             else:
-                sum_s += str(cur_sum)
+                res+=str(cur_sum)
                 carry = 0
-            i-=1
-            j-=1
-        while i>=0:
-            cur_sum = int(num1[i])+carry
+        i+=1
+        while i< len(big):
+            #print(i)
+            #print(big[i])
+            cur_sum= carry+int(big[i])
             if cur_sum >9:
-                sum_s += str(cur_sum%10)
                 carry = 1
+                res+=str(cur_sum%10)
             else:
-                sum_s += str(cur_sum)
+                res+=str(cur_sum)
                 carry = 0
-            i-=1
-        while j>=0:
-            cur_sum = int(num2[j])+carry
-            if cur_sum >9:
-                sum_s += str(cur_sum%10)
-                carry = 1
-            else:
-                sum_s += str(cur_sum)
-                carry = 0
-            j-=1
-        if carry: sum_s +="1"
-        return(sum_s[::-1])
+            i+=1
+        if carry: res+=str(carry)
+        return(res[::-1])
+            
+            
+            
+        
         
